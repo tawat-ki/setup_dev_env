226a5c2d2595b24b0ae9705fa85a6f554617105e
tawat-ki <tawat.kiate@gmail.com>
2024-01-31 23:49:42 +0700

    add package for telescope

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 set -e
 echo "alias python=python3" >> ~/.bashrc
 apt update
-apt install -y wget git 
+apt install -y wget git ripgrep fd-find
 wget --directory-prefix  /tmp https://github.com/neovim/neovim/releases/download/v0.9.5/nvim-linux64.tar.gz
 tar -xvf /tmp/nvim-linux64.tar.gz --directory=/tmp
 cp -r /tmp/nvim-linux64/ /opt/nvim
@@ -11,5 +11,4 @@ git clone https://github.com/tawat-ki/dotfiles ~/dotfiles
 mkdir -p ~/.config/
 ln -s ~/dotfiles/.config/nvim  ~/.config/nvim 
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-bash ./setup_jupyter_ascending.sh
 /usr/bin/nvim +PackerSync
--- setup_jupyter_ascending.sh
+++ setup_jupyter_ascending.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -e
 bash ~/dotfiles/ja_setup.sh
+pip install jupyterthemes
+/usr/local/bin/jt -t oceans16
 echo "alias ja='~/dotfiles/ja_make_pair.sh'" >> ~/.bashrc
